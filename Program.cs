db4327825faf4a531a1a482cc80c059a025e7121
Tsvaki <tsvakirt@gmail.com>
2025-07-30 15:52:44 +0200

    Intial Console Example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string? name = Console.ReadLine();
+if (!string.IsNullOrEmpty(name))
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+else
+{
+    Console.WriteLine("Hello, stranger!");
+}
