141f015ed049bb0993698933b4a9cac3a21a9d52
Tsvaki <tsvakirt@gmail.com>
2025-08-04 12:24:41 +0200

    No Change, Just test

--- obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f747e11c9a76ee65957e43a255315d74b79f65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4327825faf4a531a1a482cc80c059a025e7121")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
