cbdd84dff92e9efb2727505424e6b106f5eb931b
Tsvaki <tsvakirt@gmail.com>
2025-08-07 14:16:11 +0200

    Arithmetic

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 ﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("What is your name?");
-// Read user input
-string? name = Console.ReadLine();
-if (!string.IsNullOrEmpty(name))
+internal class Program
 {
-    Console.WriteLine($"Hello, {name}! This is demo code for a console application.");
-}
-else
-{
-    Console.WriteLine("Hello, stranger! Are lost ? You should not be here. Code is dangerous!");
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("What is your name?");
+        // Read user input
+        string? name = Console.ReadLine();
+        if (!string.IsNullOrEmpty(name))
+        {
+            Console.WriteLine($"Hello, {name}! This is demo code for a console application.");
+        }
+        else
+        {
+            Console.WriteLine("Hello, stranger! Are lost ? You should not be here. Code is dangerous!");
+        }
+        // Demonstrating basic arithmetic operations
+        // Variables for arithmetic operations
+        int x = 10;
+        int y = 5;
+        // Variables for string concatenation
+        string A = "abcd", B = "efgh";
+        //TODO: Add some code to demonstrate basic arithmetic operations
+        Console.WriteLine($"Sum of {x} and {y} is: {x + y}");
+        Console.WriteLine($"Concatenation of {A} and {B} is: {A + B}");
+        Console.WriteLine($"BOMDAS {y} and {x} is: {x / y * x}");
+
+        Console.WriteLine("");
+
+        Console.WriteLine("-----Short Hand ------ ");
+        Console.WriteLine($" {x} add one x++ =  {++x}");
+        Console.WriteLine($" {y} add one y-- =  {--y}"); 
+    }
 }
